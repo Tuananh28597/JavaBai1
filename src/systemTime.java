24936f1030327bfa93d18961b56bf0fb1b469f65
Tuananh28597 <86956980+Tuananh28597@users.noreply.github.com>
2021-08-04 13:51:25 +0700

    update system time

---
+++ src/systemTime.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class systemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); //lay ra thoi gian hien tai
+
+        System.out.println("Now is: " + now);
+    }
+}
